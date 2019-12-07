df0d5c094c29ee15f2791873debddf557f3875d6
xiaofe <xiaofe@tequilaguo-mb0.tencent.com>
2019-12-07 19:11:04 +0800

    wedding invitation and wishlist

--- app.js
+++ app.js
@@ -5,44 +5,66 @@
 
 var express = require('express'),
 	routes = require('./routes'),
+	favicon = require('serve-favicon'),
+	bodyParser = require('body-parser'),
+	compress = require('compression'),
+	logger = require('morgan'),
+	methodOverride = require('method-override'),
 	rt_common = require('./routes/rt_common'),
-	rt_drama = require('./routes/rt_drama'),
-	http = require('http'),
+	// rt_drama = require('./routes/rt_drama'),
 	path = require('path');
 
-var weixin = require('./routes/weixin');
+// var weixin = require('./routes/weixin');
 
 var app = express();
-
-// all environments
-app.set('port', process.env.PORT || 80);
 //app.set('views', __dirname + '/views');
 //app.set('view engine', 'ejs');
-app.use(express.favicon(__dirname + '/public/favicon.ico'));
-app.use(express.logger('dev'));
-app.use(express.bodyParser({ keepExtensions: true, uploadDir: __dirname + '/public/upload/img'}));
-app.use(express.compress());
-app.use(express.methodOverride());
-app.use(app.router);
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+// app.use(bodyParser({ keepExtensions: true, uploadDir: __dirname + '/public/upload/img'}));
+app.use(compress());
+app.use(methodOverride());
+// app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public'), {maxAge : 604800000}));
 
-// development only
-if ('development' == app.get('env')) {
-  // app.use(express.errorHandler());
-  app.use(function(err, req, res, next){
-		console.error(err.stack);
-		res.send(500, 'Something broke!');
-	});
-}
 
 app.get('/', function (req, res){
 	res.sendfile(__dirname + '/public/index.html');
 });
 app.all('/common/?*', rt_common);
-app.get('/drama', rt_drama.rt_drama);
-app.get('/weixin/interface', weixin.getToken);
-app.post('/weixin/interface', weixin.postReq);
+// app.get('/drama', rt_drama.rt_drama);
+// app.get('/weixin/interface', weixin.getToken);
+// app.post('/weixin/interface', weixin.postReq);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app
---
+++ public/wedding/list_data.js
@@ -0,0 +1,45 @@
+var ListData = [{
+		name : 'MyJuice2家用榨汁机',
+		pic : 'list_1.jpg',
+		price : '300元'
+	}, {
+		name : '日式釉下彩陶瓷餐具套装',
+		pic : 'list_2.jpg',
+		price : '300元'
+	}, {
+		name : '家用复古棉花糖机',
+		pic : 'list_3.jpg',
+		price : '350元'
+	}, {
+		name : '手动碎纸机',
+		pic : 'list_4.jpg',
+		price : '100元'
+	}, {
+		name : 'lego创意组31006',
+		pic : 'list_5.jpg',
+		price : '250元'
+	}, {
+		name : 'IKBC G-87 白色红轴',
+		pic : 'list_6.jpg',
+		price : '600元'
+	}, {
+		name : '日本药师窑锦彩和服招财猫',
+		pic : 'list_7.jpg',
+		price : '250元'
+	}, {
+		name : 'ztylus思拍乐iphone6S外置特效镜头',
+		pic : 'list_8.jpg',
+		price : '900元'
+	}, {
+		name : '北欧风全棉纯色刺绣四件套',
+		pic : 'list_9.jpg',
+		price : '350元'
+	}, {
+		name : '小善大爱－免费午餐公益捐助',
+		pic : 'list_11.jpg',
+		price : '100元起'
+	}, {
+		name : '飞利浦空气炸锅HD9232',
+		pic : 'list_10.jpg',
+		price : '1100元'
+	}]
